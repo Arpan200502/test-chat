fe3c2662e07b47023df4fd7099ae9b596efd921d
Arpan Mukherjee <arpanmukherjee255@gmail.com>
2026-01-30 22:48:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const WebSocket = require("http");
-const server = require("http").createServer();
+const WebSocket = require("ws");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("WebSocket server is running");
+});
+
 const wss = new WebSocket.Server({ server });
-const PORT = 8080;
 
+const PORT = process.env.PORT || 8080;
 
-wss.on("connection",ws =>{
-    console.log("client connected")
+wss.on("connection", (ws) => {
+  console.log("client connected");
 
-
-        ws.on("message",message=>{
-            wss.clients.forEach(client =>{
-                if(client.readyState === WebSocket.OPEN){
-                    client.send(message.toString());
-                }
-            })
-        })
-ws.on("close", () => {
-        console.log("Client disconnected");
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
     });
+  });
 
-})
+  ws.on("close", () => {
+    console.log("client disconnected");
+  });
+});
 
-
-server.listen(PORT,()=>{
-    console.log(`websocket server on http://localhost:${PORT}`)
-})
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server listening on port", PORT);
+});
